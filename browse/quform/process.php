9f3b5fa588613ccfa45ab0f9be62f4afa0dd3f3f
Travis <travis@projectcirrus.stream>
2017-04-16 21:19:02 +0100

    amended quform & added thanks page

--- browse/quform/process.php
+++ browse/quform/process.php
@@ -120,7 +120,7 @@ $config['autoreplyBody'] = '/emails/autoreply.php';
  *
  * $config['redirect'] = 'http://www.example.com/thanks.html';
  */
-$config['redirect'] = 'https://travisreynolds.co.uk/browse/thanks';
+$config['redirect'] = 'https://travisreynolds.uk/browse/thanks';
 
 /**
  * Whether or not to save the form data to a database. true or false
